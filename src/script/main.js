6c2c6a7d7cdc983563c12a988e264cf55b55f1ac
Cristiano do Prado <82547379+Cris-Prado@users.noreply.github.com>
2024-12-15 18:28:29 -0400

    carossel-menu

--- src/script/main.js
+++ src/script/main.js
@@ -1,3 +1,5 @@
+// Botão de Navegaão da Página 
+
 const hamburger = document.querySelector(".mobile");
 const navMenu = document.querySelector(".list");
 
@@ -5,3 +7,19 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
 });
+
+// Slide Do Cardapio
+let cont = 1
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    nextImg();
+}, 4000)
+
+function nextImg(){
+    cont++
+    if(cont>4){
+        cont=1
+    }
+    document.getElementById("radio"+cont).checked = true;
+}
