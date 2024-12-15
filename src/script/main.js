362ddb43fdbc069220c0c641cdecdbcbc57b693c
Cristiano do Prado <82547379+Cris-Prado@users.noreply.github.com>
2024-12-14 21:13:54 -0400

    Box-ICons

--- src/script/main.js
+++ src/script/main.js
@@ -1,1 +1,7 @@
-alert("Olá Mundi")
+const hamburger = document.querySelector(".mobile");
+const navMenu = document.querySelector(".list");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
